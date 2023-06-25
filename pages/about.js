6d0189119683e5406c0ab121281458cc07f3cc10
kuum97 <kuum97@icloud.com>
2023-06-26 00:35:12 +0900

    styled about page

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,29 @@
 export default function About() {
   return (
-    <div>
-      <h1>About us</h1>
-      <p>
-        Welcome to the official explorer for The New York Times Best Seller list
-        explorer.
-        <br />
-        We hope you enjoy your stay.
-      </p>
-    </div>
+    <>
+      <div className="box">
+        <div className="p">
+          <h1>About us</h1>
+          <p>
+            Welcome to the official explorer for The New York Times Best Seller
+            list explorer.
+            <br />
+            We hope you enjoy your stay.
+          </p>
+        </div>
+      </div>
+      <style jsx>{`
+        h1 {
+          font-size: larger;
+        }
+        .box {
+          display: flex;
+          justify-content: center;
+        }
+        .p {
+          width: 500px;
+        }
+      `}</style>
+    </>
   );
 }
